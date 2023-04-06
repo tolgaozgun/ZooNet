0679cf7ed6f6120a7209b2a0337fec0d05caf8a3
Selim Can Guler <selimcangler@gmail.com>
2023-04-07 02:04:09 +0300

    fix

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # TODO: add optimizer and loss_fn options to parameters
 def train(train_data_loader, val_data_loader, train_steps: int, validation_steps: int, num_channels: int=3, classes: int=1, learning_rate:float=0.01, epochs: int=10):
     print("Initializing the ZooNet Model...")
+    print("cuda" if torch.cuda.is_available() else "cpu")
     model = ZooNet(num_channels, classes).to(device)
 
     print("Initializing the optimizer and loss function")
@@ -54,11 +55,24 @@ def train(train_data_loader, val_data_loader, train_steps: int, validation_steps
             train_correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
 
         # Loop over validation set
+        with torch.no_grad():
+            model.eval()
+
+            for (x, y) in val_data_loader:
+                (x, y) = (x.to(device), y.to(device))
+
+                # Make prediction
+                prediction = model(x)
+                loss = loss_fn(prediction, y)
+                validation_loss += loss
+
+                # Calculate the number of correct predictions 
+                validation_correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
 
         avg_train_loss = train_loss / train_steps
         avg_validation_loss = validation_loss / validation_steps
         train_correct_accuracy = train_correct / len(train_data_loader.dataset)
-        validation_correct_accuracy = train_correct / len(train_data_loader.dataset)
+        validation_correct_accuracy = validation_correct / len(val_data_loader.dataset)
 
         training_history['train_loss'].append(avg_train_loss)
         training_history['train_accuracy'].append(train_correct_accuracy)
@@ -69,19 +83,6 @@ def train(train_data_loader, val_data_loader, train_steps: int, validation_steps
         print(f"\tTrain loss: {avg_train_loss}, Train accuracy: {train_correct_accuracy}")
         print(f"\tValidation loss: {avg_validation_loss}, Validation accuracy: {validation_correct_accuracy}")
 
-    with torch.no_grad():
-        model.eval()
-
-        for (x, y) in val_data_loader:
-            (x, y) = (x.to(device), y.to(device))
-
-            # Make prediction
-            prediction = model(x)
-            loss = loss_fn(prediction, y)
-            validation_loss += loss
-
-            # Calculate the number of correct predictions 
-            validation_correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
 
     end_time = time.time()
     print("Finished training.")
