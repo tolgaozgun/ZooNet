2e114def63b9abb132a1ca02f42a824f99f261de
Tolga <tolgaozgunn@gmail.com>
2023-04-06 15:50:33 +0000

    Initial project setup

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import logging
 import torch
-import torchvision
-from model import ZooNet
+from torch.utils.data import random_split, DataLoader
+from torchvision.transforms import transforms
+from torchvision.datasets import ImageNet
 from settings import Settings
 
-if __name__ == '__main__':
-    main()
 
 
 # Training hyperparameters
 LEARNING_RATE = 1e-3
 BATCH_SIZE = 64
 EPOCHS = 10
-
-# Data split
-TRAIN_SPLIT = 0.75
-VAL_SPLIT = 1 - TRAIN_SPLIT
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -27,13 +22,14 @@ def main():
     
     # Load the dataset, and split the dataset to train, validation, and test
     logger.info("Loading the dataset...")
-    train_data = None
-    test_data = None
+    train_data = ImageNet('./data', split='train', transform=transforms.ToTensor())
+    val_data = ImageNet('./data', split='val', transform=transforms.ToTensor())
+
 
     # Initialize  data loaders
     logger.info("Initializing data loaders...")
-    train_data_loader = None # TODO:
-    val_data_loader = None # TODO:
+    train_data_loader = DataLoader(train_data, shuffle=True, batch_size=BATCH_SIZE) 
+    val_data_loader = DataLoader(val_data, batch_size=BATCH_SIZE) 
 
     # Calculate steps per epoch
     train_steps = len(trainDataLoader.dataset) // BATCH_SIZE
@@ -41,7 +37,10 @@ def main():
 
     # Train the model
     logger.info("Initializing the model...")
-    model = ZooNet(
-        num_channels=3,
-        classes=len(train_data.dataset.classes)
-    ).to(device)
+    # model = ZooNet(
+    #     num_channels=3,
+    #     classes=len(train_data.dataset.classes)
+    # ).to(device)
+
+if __name__ == '__main__':
+    main()
