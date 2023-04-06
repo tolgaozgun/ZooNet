4ab3f801a0ed9df99acbb809df1ea18da91779ac
Tolga Özgün <tolgaozgunn@gmail.com>
2023-04-06 22:44:16 +0000

    sync

--- main.py
+++ main.py
@@ -1,46 +1,65 @@
 import logging
 import torch
+import numpy
 from torch.utils.data import random_split, DataLoader
 from torchvision.transforms import transforms
-from torchvision.datasets import ImageNet
+from torchvision.datasets import ImageFolder
 from settings import Settings
-
-
+from train import train
 
 # Training hyperparameters
 LEARNING_RATE = 1e-3
 BATCH_SIZE = 64
 EPOCHS = 10
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+train_dir = "./data/train"
+val_dir = "./data/val"
+
+def normalize_transform():
+    return transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
 
 def main():
-    logger = logging.getLogger(__name__)
-    logger.info("Loading settings...")
+    print("Loading settings...")
     settings = Settings()
     settings.load_from_file()
     
     # Load the dataset, and split the dataset to train, validation, and test
-    logger.info("Loading the dataset...")
-    train_data = ImageNet('./data', split='train', transform=transforms.ToTensor())
-    val_data = ImageNet('./data', split='val', transform=transforms.ToTensor())
-
+    print("Loading the dataset...")
+    train_data = ImageFolder(
+        train_dir,
+        transforms.Compose([
+            transforms.RandomResizedCrop(128),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize_transform()
+        ])
+    )
+    train_data = torch.utils.data.Subset(train_data, numpy.random.choice(len(train_data), 1000, replace=False))
+    val_data = ImageFolder(
+        val_dir,
+        transforms.Compose([
+            transforms.RandomResizedCrop(128),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize_transform()
+        ])
+    )
+    val_data = torch.utils.data.Subset(val_data, numpy.random.choice(len(val_data), 100, replace=False))
 
     # Initialize  data loaders
-    logger.info("Initializing data loaders...")
+    print("Initializing data loaders...")
     train_data_loader = DataLoader(train_data, shuffle=True, batch_size=BATCH_SIZE) 
     val_data_loader = DataLoader(val_data, batch_size=BATCH_SIZE) 
 
     # Calculate steps per epoch
-    train_steps = len(trainDataLoader.dataset) // BATCH_SIZE
-    val_steps = len(valDataLoader.dataset) // BATCH_SIZE
+    train_steps = len(train_data_loader.dataset) // BATCH_SIZE
+    val_steps = len(val_data_loader.dataset) // BATCH_SIZE
+    print(f"Size of train set: {len(train_data_loader.dataset)}")
+    print(f"Size of validation set: {len(val_data_loader.dataset)}")
 
     # Train the model
-    logger.info("Initializing the model...")
-    # model = ZooNet(
-    #     num_channels=3,
-    #     classes=len(train_data.dataset.classes)
-    # ).to(device)
+    train(train_data_loader, val_data_loader, train_steps, val_steps, classes=11)
 
-if __name__ == '__main__':
-    main()
+
+main()
--- train.py
+++ train.py
@@ -1,17 +1,17 @@
 from model import ZooNet
 from torch.optim import Adam
 from torch import nn
+import torch
 import time
-import logging
 
-logger = logging.getLogger(__name__)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # TODO: add optimizer and loss_fn options to parameters
-def train(train_data_loader, validation_data_loader, train_steps: int, validation_steps: int, num_channels: int=3, classes: int=1, learning_rate:float=0.1, epochs: int=10):
-    logger.info("Initializing the ZooNet Model...")
+def train(train_data_loader, val_data_loader, train_steps: int, validation_steps: int, num_channels: int=3, classes: int=1, learning_rate:float=0.01, epochs: int=10):
+    print("Initializing the ZooNet Model...")
     model = ZooNet(num_channels, classes).to(device)
 
-    logger.info("Initializing the optimizer and loss function")
+    print("Initializing the optimizer and loss function")
     optimizer = Adam(model.parameters(), lr=learning_rate)
     loss_fn = nn.NLLLoss()
     
@@ -23,7 +23,7 @@ def train(train_data_loader, validation_data_loader, train_steps: int, validatio
         "validation_accuracy": []
     }
 
-    logger.info("Training the network...")
+    print("Training the network...")
     start_time = time.time()
 
     for e in range(epochs):
@@ -37,7 +37,7 @@ def train(train_data_loader, validation_data_loader, train_steps: int, validatio
         validation_correct = 0
 
         # Loop over training set
-        for (x, y) in training_data_loader:
+        for (x, y) in train_data_loader:
             (x, y) = (x.to(device), y.to(device))
 
             # Forwards propagation
@@ -54,22 +54,9 @@ def train(train_data_loader, validation_data_loader, train_steps: int, validatio
             train_correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
 
         # Loop over validation set
-        with torch.no_grad():
-            model.eval()
-
-            for (x, y) in validation_data_loader:
-                (x, y) (x.to(device), y.to(device))
-
-                # Make prediction
-                prediction = model(x)
-                loss = loss_fn(predicton, y)
-                validation_loss += loss
-
-                # Calculate the number of correct predictions 
-                validation_correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
 
         avg_train_loss = train_loss / train_steps
-        avg_validation_loss = validation_loss / validatin_steps
+        avg_validation_loss = validation_loss / validation_steps
         train_correct_accuracy = train_correct / len(train_data_loader.dataset)
         validation_correct_accuracy = train_correct / len(train_data_loader.dataset)
 
@@ -78,10 +65,26 @@ def train(train_data_loader, validation_data_loader, train_steps: int, validatio
         training_history['validation_loss'].append(avg_validation_loss)
         training_history['validation_accuracy'].append(validation_correct_accuracy)
 
-        logger.info(f"EPOCH: {e+1}/{epochs}")
-        logger.info(f"\tTrain loss: {avg_train_loss}, Train accuracy: {train_correct_accuracy}")
-        logger.info(f"\tValidation loss: {avg_validation_loss}, Validation accuracy: {validation_correct_accuracy}")
+        print(f"EPOCH: {e+1}/{epochs}")
+        print(f"\tTrain loss: {avg_train_loss}, Train accuracy: {train_correct_accuracy}")
+        print(f"\tValidation loss: {avg_validation_loss}, Validation accuracy: {validation_correct_accuracy}")
+
+    with torch.no_grad():
+        model.eval()
+
+        for (x, y) in val_data_loader:
+            (x, y) = (x.to(device), y.to(device))
+
+            # Make prediction
+            prediction = model(x)
+            loss = loss_fn(prediction, y)
+            validation_loss += loss
+
+            # Calculate the number of correct predictions 
+            validation_correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
 
     end_time = time.time()
-    logger.info("Finished training.")
-    logger.info(f"Training duratin: {start_time - end_time}")
+    print("Finished training.")
+    print(f"Training duratin: {start_time - end_time}")
+
+    torch.save(model, './out')
